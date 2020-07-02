42d25beef2da747b8a820d44f849efbfe9d6dcc9
Ask Attilla Dagestad Strande <aska.d.s@hotmail.com>
2020-07-03 00:52:52 +0200

    added fake data

--- src/App.js
+++ src/App.js
@@ -1,28 +1,83 @@
-import React from 'react';
-import './App.css';
-import { Component } from 'react';
+import React from "react";
+import "./App.css";
+import { Component } from "react";
 
-let defaultTextColor = "black";
 let defaultStyle = {
-  color: defaultTextColor
+  color: "black",
+};
+
+let fakeServerData = {
+  user: {
+    name: "Ask",
+    playlists: [
+      {
+        name: "My favorites",
+        songs: [
+          { name: "Beat It", duration: 1234 },
+          { name: "Cannelioni Makaroni", duration: 1032 },
+          { name: "Rosa helikopter", duration: 749 },
+        ],
+      },
+      {
+        name: "My favorites",
+        songs: [
+          { name: "Beat It", duration: 1234 },
+          { name: "Cannelioni Makaroni", duration: 1032 },
+          { name: "Rosa helikopter", duration: 749 },
+        ],
+      },
+      {
+        name: "My favorites",
+        songs: [
+          { name: "Beat It", duration: 1234 },
+          { name: "Cannelioni Makaroni", duration: 1032 },
+          { name: "Rosa helikopter", duration: 749 },
+        ],
+      },
+      {
+        name: "My favorites",
+        songs: [
+          { name: "Beat It", duration: 1234 },
+          { name: "Cannelioni Makaroni", duration: 1032 },
+          { name: "Rosa helikopter", duration: 749 },
+        ],
+      },
+    ],
+  },
+};
+
+class PlaylistCounter extends Component {
+  render() {
+    return (
+      <div style={{ ...defaultStyle, width: "40%", display: "inline-block" }}>
+        <h2>{this.props.playlists.length} playlists</h2>
+      </div>
+    );
+  }
 }
 
-class Aggregate extends Component {
-  render(){
-    return(
-      <div style={{...defaultStyle, width:"40%", display: "inline-block"}}>
-        <h2>Number Text</h2>
+class HoursCounter extends Component {
+  render() {
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+      return songs.concat(eachPlaylist.songs);
+    }, []);
+    let totalDuration = allSongs.reduce((sum, eachSong) => {
+      return sum + eachSong.duration;
+    }, 0);
+    return (
+      <div style={{ ...defaultStyle, width: "40%", display: "inline-block" }}>
+        <h2>{Math.round(totalDuration / 60)} hours</h2>
       </div>
     );
   }
 }
 
 class Filter extends Component {
-  render(){
+  render() {
     return (
       <div style={defaultStyle}>
-        <img alt="Search"/>
-        <input type="text"/>
+        <img alt="Search" />
+        <input type="text" />
         Filter
       </div>
     );
@@ -30,9 +85,9 @@ class Filter extends Component {
 }
 
 class Playlist extends Component {
-  render(){
-    return(
-      <div style={{...defaultStyle, display: "inline-block", width: "25%"}}>
+  render() {
+    return (
+      <div style={{ ...defaultStyle, display: "inline-block", width: "25%" }}>
         <img />
         <h3>Playlist name</h3>
         <ul>
@@ -45,19 +100,40 @@ class Playlist extends Component {
   }
 }
 
-function App() {
-  return (
-    <div className="App" style={defaultStyle}>
-      <h1>Title</h1>
-      <Aggregate />
-      <Aggregate />
-      <Filter />
-      <Playlist />
-      <Playlist />
-      <Playlist />
-      <Playlist />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { serverData: {} };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ serverData: fakeServerData });
+    }, 2000);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.serverData.user ? (
+          <div>
+            <h1 style={{ ...defaultStyle, "font-size": "54px" }}>
+              {this.state.serverData.user.name}'s playlists
+            </h1>
+            <PlaylistCounter playlists={this.state.serverData.user.playlists} />
+            <HoursCounter playlists={this.state.serverData.user.playlists} />
+            <Filter />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+          </div>
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
